6940ca8e4b52b634def52ef8eb4d5817e3960e76
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-25 18:38:16 +0100

    working on events model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,15 @@ class SessionsController < ApplicationController
 		user = User.find_by(name: params[:session][:name].downcase)
 		if user
 			log_in user
-			redirect_to user
+			redirect_to root_path
 		else
 			message = "User not found"
 			flash[:warning] = message
 			render "new"
 		end
 	end
+
+	def destroy
+		log_out if logged_in?
+		redirect_to root_path
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,13 +3,16 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	def log_out
+		session.delete(:user_id)
+    @current_user = nil
+  end
+
 	def logged_in?
-		!current_user.nil?
+		!!current_user
 	end
 
-	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-		end
+	def current_user	
+		@current_user ||= User.find_by(id: session[:user_id])
 	end
 end
